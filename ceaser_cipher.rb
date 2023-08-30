78dd6588b109376df98061172f33ae673e0b290f
suman tiwari <tiwarisuman80@gmail.com>
2023-08-30 08:16:26 +0545

    Refactored cipher method

--- ceaser_cipher.rb
+++ ceaser_cipher.rb
@@ -1,31 +1,22 @@
-def self.ceaser_cipher(str, index)
-  down_lower, down_upper, up_lower, up_upper = 97, 122, 65, 90
-  down_range = (down_lower..down_upper).to_a
-  up_range = (up_lower..up_upper).to_a
-  val_range = down_range.concat(up_range)
+def test_ceaser_cipher(str, shift)
+  alphabet = ('a'..'z').to_a
   new_str = ''
   str.split('').each do |charr|
-    ascii_val = charr.ord
-    unless val_range.include?(ascii_val)
-      new_str << charr
-    else
-      new_ascii_val = ascii_val + index
-      if val_range.include?(new_ascii_val)
-        new_str << new_ascii_val.chr
+    down_charr = charr.downcase
+    if alphabet.include?(down_charr)
+      index = alphabet.index(down_charr)
+      index = (index + shift)%26
+
+      if down_charr == charr
+        new_str << alphabet[index]
       else
-        down_upper_diff = new_ascii_val - down_upper
-        up_upper_diff = new_ascii_val - up_upper
-        
-        if down_upper_diff < up_upper_diff
-           new_ascii_val = (down_lower + down_upper_diff-1)
-        else
-           new_ascii_val = (up_lower + up_upper_diff - 1)
-        end
-        new_str << new_ascii_val.chr        
+        new_str << alphabet[index].upcase
       end
+    else
+      new_str << charr
     end
   end
-  return new_str
+  new_str
 end
 
-puts ceaser_cipher("What is your name?", 5) #'Bmfy nx dtzw sfrj?'
+puts test_ceaser_cipher("What is your name?", 5) #'Bmfy nx dtzw sfrj?'
